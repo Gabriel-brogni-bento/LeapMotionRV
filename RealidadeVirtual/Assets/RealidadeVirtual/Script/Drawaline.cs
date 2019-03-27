515b9d501d938be8f76c9dc8564825de5a0b7f1c
Gabriel-brogni-bento <gabrielbento98@gmail.com>
2019-03-27 17:23:12 -0300

    Commit inicial do projeto

---
+++ RealidadeVirtual/Assets/RealidadeVirtual/Script/Drawaline.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class Drawaline : MonoBehaviour
+{
+    public GameObject trail;
+
+    public GameObject Finger;
+
+    public GameObject CurrentTrail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StopDraw()
+    {
+        StopAllCoroutines();
+    }
+
+    public void instatiateTrail()
+    {
+        CurrentTrail = Instantiate(trail, Finger.transform.position, Quaternion.identity);
+        StartCoroutine(Draw());
+    }
+
+    IEnumerator Draw()
+    {
+        while (true)
+        {
+            CurrentTrail.transform.position = new Vector3(Finger.transform.position.x, Finger.transform.position.y, Finger.transform.position.z);
+            yield return null;
+        }
+        
+    }
+
+}
